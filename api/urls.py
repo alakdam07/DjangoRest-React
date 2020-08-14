a5ce21cc26316a63c343f5125edc57dc12e0b829
alak <alak@estatelogs.com>
2020-08-14 10:39:12 +0300

    database setup :rocket:

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,10 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview" )
+    path('', views.apiOverview, name="api-overview" ),
+    path('task-lists/', views.taskList, name="task-list" ),
+    path('task-detail/<str:pk>/', views.taskDetail, name="single-task"),
+    path('task-create/', views.taskCreate, name="task-create"),
+    path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
+    path('task-delete/<str:pk>/', views.taskDelete, name="task-delete")
 ]
